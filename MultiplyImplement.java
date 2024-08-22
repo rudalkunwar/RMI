9c2b86313a45ff1f8603145418672981641b2032
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-08-22 13:27:50 +0545

    some fixes

--- MultiplyImplement.java
+++ MultiplyImplement.java
@@ -3,7 +3,7 @@ import java.rmi.*;
 import java.rmi.server.*;
 public class MultiplyImplement extends UnicastRemoteObject implements Multiply {
     
-    Multiply() throws RemoteException{
+    MultiplyImplement() throws RemoteException{
         super();
     } 
     public int doMultiply(int x , int y) throws RemoteException{
